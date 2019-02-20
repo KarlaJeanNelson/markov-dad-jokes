67667f5211c45f140f35425d693aec26ae711b5a
KarlaJeanNelson <karla.jean.nelson@gmail.com>
2019-02-20 03:01:19 -0600

    test new map

--- server/db.js
+++ server/db.js
@@ -9,19 +9,21 @@ let mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/dadjokes';
 
 mongoose.connect(mongoURI, { useNewUrlParser: true, useCreateIndex: true });
 
+// Once connected, check to make sure there is data in the database.
+// If not, get data from the joke API, and save it to the database.
 mongoose.connection.on('connected', () => {
 	console.log(`Connected to database ${mongoURI}.`)
-	getData();
 })
-// .then(() => getStringData())
+.then(() => checkForData())
 .catch(e => console.log(`Error with mongoose connection.`, e))
-
 
 mongoose.connection.on('error', (err) => {
 	console.log(`Mongo connection error.`, err)
 })
 
-const getData = (page = 1, jokeData = []) => {
+const lookupTable = new Map();
+const jokeSeeds = [];
+const saveData = (page = 1, jokeData = []) => {
 	// console.log(page);
 	const config = {
 		headers: {
@@ -37,20 +39,83 @@ const getData = (page = 1, jokeData = []) => {
 	.then(({data}) => {
 		const retrievedJokes = jokeData.concat(data.results)
 		if (data.current_page === data.total_pages) {
-			const errors = [];
-			const updates = [];
 			retrievedJokes.forEach(item => {
 				const { joke } = item;
 				// console.log(joke);
-				Joke.findOneAndUpdate({ id: item.id }, { joke }, { upsert: true }, (err, doc) => (
-					err ? errors.push(err) : updates.push(doc)
-				))
+				Joke.findOneAndUpdate({ id: item.id }, { joke }, { upsert: true })
+				.then(result => getTuples(result.joke))
+				.catch(e => console.log(e))
 			})
 			// console.log(`${updates.length} updates, ${errors.length} errors.`);
 		} else {
 			page++;
-			getData(page, retrievedJokes);
+			saveData(page, retrievedJokes);
 		}
 	})
 	.catch(e => console.log(`getData error page ${page}`, e))
+}
+
+const checkForData = () => {
+	Joke.countDocuments({}, (err, count) => {
+		console.log(count);
+		if (count < 500) {
+			saveData();
+		}
+	})
+	
+	Joke.findOne({})
+	.then(result => testTupleGen(result))
+}
+
+const getTuples = (joke, order = 3) => {
+	Joke.findOne({})
+	// .then(result => console.log(result))
+}
+
+const testTupleGen = (jokeDoc) => {
+	console.log(`in testTupleGen`);
+	const { joke } = jokeDoc;
+	let text = joke.replace(/[^\w\s]|_/g, $1 => ' ' + $1).replace(/[ ]+/g, ' ').split(' ');
+	// console.log(text);
+	const myLookupTable = getLookupTable(text)
+	console.log(`lookupTable:`, myLookupTable);
+	let seed = jokeSeeds[random(jokeSeeds.length - 1)];
+	seed = seed.join(' ')
+	console.log(`seed:`, seed);
+	const options = myLookupTable.filter(item => item.gram === seed)
+	let getNext = options[random(options.length - 1)]
+	console.log(getNext.nextWd);
+	getNextWord(seed);
+}
+
+const getLookupTable = (text, order = 2) => {
+	console.log(text);
+	const myArr = [];
+	for (let i = 0; i <= text.length - order; i++) {
+		let gram = text.slice(i, i + order);
+		let nextWd = 'END';
+		if (i === 0) {
+			jokeSeeds.push(gram)
+			console.log(`jokeSeeds:`, jokeSeeds);
+		}
+		
+		if (i === text.length - order) {
+			lookupTable.set(gram, 'END')
+		} else {
+			nextWd = text[i + order]
+			lookupTable.set(gram, text[i + order])
+		}
+		myArr.push({gram: gram.join(' '), nextWd})
+	}
+	return myArr;
+}
+
+const getNextWord = input => {
+	console.log(`input:`, input);
+}
+
+const random = maxNum => (Math.floor(Math.random() * maxNum));
+
+const getNext = (lookupTable, current, sentence = []) => {
+	lookupTable.filter(item => Object.keys(item) === current)
 }
